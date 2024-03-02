cc61b5abd9dfae9e0e7dfdf989f4de85093ed8db
restrepoTorres <vmanuel.restrepo@udea.edu.co>
2024-03-01 22:11:58 -0500

    lab día 2.

--- src/main/java/com/udea/vuelo/VueloApplication.java
+++ src/main/java/com/udea/vuelo/VueloApplication.java
@@ -2,6 +2,10 @@ package com.udea.vuelo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
 
 @SpringBootApplication
 public class VueloApplication {
@@ -10,4 +14,18 @@ public class VueloApplication {
 		SpringApplication.run(VueloApplication.class, args);
 	}
 
+
+
+
+@Bean
+public CorsFilter corsFilter(){
+	UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+	CorsConfiguration config = new CorsConfiguration();
+	config.setAllowCredentials(true);
+	config.addAllowedOrigin("http://localhost:3001");
+	config.addAllowedHeader("*");
+	config.addAllowedMethod("*");
+	source.registerCorsConfiguration("/**",config);
+	return new CorsFilter(source);
+}
 }
